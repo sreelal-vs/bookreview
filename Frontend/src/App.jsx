7ece8ef546c1ae3a41c05c8245f8cfac6e16ad0e
Sreelal <sreelalseven@gmail.com>
2026-07-15 20:29:00 +0530

    third commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home';
+
+
 function App() {
   
 
   return (
-    <>
+    <div className='w-100'>
+      <BrowserRouter>
+      <Header/>
+     <Routes>
+       <Route path='/home' element={<Home/>}/>
 
-     </>
+     </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
